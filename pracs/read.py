14f8cb803449972c623066e9b71368877d493fdc
Aditya <62930521+adityar224@users.noreply.github.com>
2024-02-18 02:09:40 -0500

    📝 updated username

---
+++ pracs/read.py
@@ -0,0 +1,4 @@
+f=open("input.txt","r")
+rl=f.read()
+print(rl)
+f.close()
---
+++ pracs/write.py
@@ -0,0 +1,4 @@
+w = open("input.txt", "w")
+w.write("Hello World my name is Aditya\nI am 21 years old\n")
+w.write("This is a sample input file to\nlearn python file handling")
+w.close()
